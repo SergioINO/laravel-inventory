ba9936cf8268d3b708e32b8c91d58564a1583b2b
Sergio Inostroza Mardones <sinostroza@ing.ucsc.cl>
2021-08-24 16:34:12 -0400

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -44,7 +44,7 @@ class ProductController extends Controller
     public function store(ProductRequest $request, Product $product)
     {
 
-        dd($request);
+        // dd($request);
         $rules = [
             'name' => 'required|string',
             'thickness' => 'required|numeric',
@@ -76,8 +76,10 @@ class ProductController extends Controller
                     $store->thickness        = $request->thickness;
                     $store->width       = $request->width;
                     $store->length = $request->length;
-                    // $store->m2 = falta hacer formula  
-                    // $store->m3 = falta hacer formula 
+                    // obtengo m2 de cada producto
+                    $store->m2 = ($request->width * $request->length)/1000;  
+                    // obtengo m3 de cada producto
+                    $store->m3 = ($request->thickness * $request->width * $request->length)/1000; 
                     // $store->PT = falta hacer formula  
                     // $store->TOTAL_PT = falta hacer formula 
                 }
@@ -132,9 +134,16 @@ class ProductController extends Controller
      */
     public function edit(Product $product)
     {
-        $categories = ProductCategory::all();
+        $product = Product::on(session()->get('database'))->find($product->id);
 
-        return view('inventory.products.edit', compact('product', 'categories'));
+        if ($product) {
+            $categories = ProductCategory::all();
+            return view('inventory.products.edit', compact('product', 'categories'));
+            
+        }
+
+        return response()->json([ 'success' => false, 'error' => 'Not unit'], 500);
+        
     }
 
     /**
@@ -146,11 +155,71 @@ class ProductController extends Controller
      */
     public function update(ProductRequest $request, Product $product)
     {
-        $product->update($request->all());
 
-        return redirect()
+        // dd($request);
+        $rules = [
+            'name' => 'required|string',
+            'thickness' => 'required|numeric',
+            'width'     => 'required|numeric',
+            'length'    => 'required|numeric',
+            'stock' => 'required|numeric',
+            'stock_defective' => 'required|numeric',
+            'purchase_price' => 'required|numeric',
+            'selling_price' => 'required|numeric',
+            'description' => 'nullable|string',
+        ];
+
+        try {
+        
+            $this->validate($request, $rules);
+
+            DB::connection(session()->get('database'))->beginTransaction();
+
+            $store = Product::on(session()->get('database'))->find($product->id);
+            $store->setConnection(session()->get('database'));
+                $store->name        = $request->name;
+                $store->product_category_id = $request->product_category_id;
+                $store->stock       = $request->stock;
+                $store->stock_defective = $request->stock_defective;
+                $store->purchase_price = $request->purchase_price;
+                $store->selling_price = $request->selling_price;
+                $store->description = $request->description;
+                if ($request->type_measure = 'mm') {
+                    $store->thickness        = $request->thickness;
+                    $store->width       = $request->width;
+                    $store->length = $request->length;
+                    // obtengo m2 de cada producto
+                    $store->m2 = ($request->width/1000) * ($request->length*304.8/1000);  
+                    // obtengo m3 de cada producto
+                    $store->m3 = ($request->thickness/1000) * ($request->width/1000) * ($request->length*304.8/1000); 
+                    // $store->PT = falta hacer formula  
+                    // $store->TOTAL_PT = falta hacer formula 
+                }
+                if ($request->type_measure = 'pulg') {
+                    $store->thickness        = $request->thickness;
+                    $store->width       = $request->width;
+                    $store->length = $request->length;
+                    // $store->m2 = falta hacer formula  
+                    // $store->m3 = falta hacer formula  
+                    // $store->PT = falta hacer formula  
+                    // $store->TOTAL_PT = falta hacer formula 
+                }
+
+            $store->save();
+                
+            DB::connection(session()->get('database'))->commit();
+
+            return redirect()
             ->route('products.index')
             ->withStatus('Producto actualizado con éxito!.');
+
+        }catch (ValidationException $exception) {
+            
+            DB::connection(session()->get('database'))->rollback();
+            return response()->json(['errors' => $exception->errors()], 422);
+        }
+
+        
     }
 
     /**
