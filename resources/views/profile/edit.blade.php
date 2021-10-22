aa9f74380ed07a9761fd785fa765932205d94d48
Sergio Inostroza Mardones <sinostroza@ing.ucsc.cl>
2021-10-22 17:24:50 -0300

    update pdf

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -75,7 +75,7 @@
                             <div class="block block-three"></div>
                             <div class="block block-four"></div>
                             <a href="#">
-                                <img class="avatar" src="{{ asset('assets/img/PACFOR.jpg') }}" alt="">
+                                <img class="avatar" src="{{ asset('assets/img/PACFOR.png') }}" alt="">
                                 <h5 class="title">{{ auth()->user()->name }}</h5>
                             </a>
                             <p class="description"></p>
