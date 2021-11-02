4dd0382e12a6d217e736b19214e60fb0067daa82
Gabriel Drápela <56370678+Gdrapela@users.noreply.github.com>
2021-11-02 10:41:09 -0300

    Create DispatchController.php

---
+++ app/Http/Controllers/DispatchController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
+
+class DispatchController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+       // return view('dispatch.index', compact('dispatch'));
+       //return redirect()->route('dispatch.index');
+       return view('dispatch.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    // public function create()
+    // {
+    //     return view('clients.create');
+    // }
+
+    // /**
+    //  * Store a newly created resource in storage.
+    //  *
+    //  * @param  \App\Http\Request\ClientRequest  $request
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function store(ClientRequest $request, Client $client)
+    // {
+    //     $client->create($request->all());
+        
+    //     return redirect()->route('clients.index')->withStatus('Cliente registrado exitosamente!');
+    // }
+
+    // /**
+    //  * Display the specified resource.
+    //  *
+    //  * @param  int  $id
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function show(Client $client)
+    // {
+    //     return view('clients.show', compact('client'));
+    // }
+
+    // /**
+    //  * Show the form for editing the specified resource.
+    //  *
+    //  * @param  int  $id
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function edit(Client $client)
+    // {
+    //     return view('clients.edit', compact('client'));
+    // }
+
+    // /**
+    //  * Update the specified resource in storage.
+    //  *
+    //  * @param  \App\Http\Request\ClientRequest  $request
+    //  * @param  int  $id
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function update(ClientRequest $request, Client $client)
+    // {
+    //     $client->update($request->all());
+
+    //     return redirect()
+    //         ->route('clients.index')
+    //         ->withStatus('Cliente modificado exitosamente.');
+    // }
+
+    // /**
+    //  * Remove the specified resource from storage.
+    //  *
+    //  * @param  int  $id
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function destroy(Client $client)
+    // {
+    //     $client->delete();
+
+    //     return redirect()
+    //         ->route('clients.index')
+    //         ->withStatus('Cliente removido exitosamente!.');
+    // }
+
+    // public function addtransaction(Client $client)
+    // {
+    //     $payment_methods = PaymentMethod::all();
+
+    //     return view('clients.transactions.add', compact('client','payment_methods'));
+    // }
+} 
