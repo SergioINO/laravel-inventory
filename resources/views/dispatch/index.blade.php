e15115a9b40986043a11dbe12a4c48a3ce865526
Gabriel Drápela <56370678+Gdrapela@users.noreply.github.com>
2021-11-02 10:41:19 -0300

    Create index.blade.php

---
+++ resources/views/dispatch/index.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app', ['page' => 'Dispatch', 'pageSlug' => 'dispatch', 'section' => 'dispatch'])
+
+@section('content')
+<div class="row">
+        <div class="col-md-12">
+            <div class="card ">
+                <div class="card-header">
+                    <div class="row">
+                        <div class="col-8">
+                            <h4 class="card-title">Listado de DESPACHOS</h4>
+                        </div>
+                        <div class="col-4 text-right">
+                            <a href="{{ route('clients.create') }}" class="btn btn-sm btn-primary">descargarPDF Despacho</a>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="card-footer py-4">
+                    <nav class="d-flex justify-content-end" aria-label="...">
+                       
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
