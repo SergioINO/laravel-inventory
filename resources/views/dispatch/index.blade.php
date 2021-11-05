ed3cf6fd776336766e180b92f3a8d4760689fbbe
Gabriel Drápela <56370678+Gdrapela@users.noreply.github.com>
2021-11-05 15:23:29 -0300

    Update index.blade.php

--- resources/views/dispatch/index.blade.php
+++ resources/views/dispatch/index.blade.php
@@ -1,25 +1,50 @@
-@extends('layouts.app', ['page' => 'Dispatch', 'pageSlug' => 'dispatch', 'section' => 'dispatch'])
+@extends('layouts.app', ['page' => 'Despacho', 'pageSlug' => 'dispatch', 'section' => 'dispatch'])
 
 @section('content')
 <div class="row">
         <div class="col-md-12">
             <div class="card ">
                 <div class="card-header">
-                    <div class="row">
+                <div class="row">
                         <div class="col-8">
-                            <h4 class="card-title">Listado de DESPACHOS</h4>
-                        </div>
-                        <div class="col-4 text-right">
-                            <a href="{{ route('clients.create') }}" class="btn btn-sm btn-primary">descargarPDF Despacho</a>
+                            <h4 class="card-title">Listado de despachos</h4>
                         </div>
                     </div>
                 </div>
                 
-                <div class="card-footer py-4">
-                    <nav class="d-flex justify-content-end" aria-label="...">
-                       
-                    </nav>
+                <div class="card-body">
+                    <div class="">
+                        <table class="table tablesorter " id="">
+                            <thead class=" text-primary">
+                                <th>Nombre</th>
+                                <th>Email</th>
+                                <th>Telefono</th>
+                                <th>Dirección</th>
+                                <th>Fecha estimada</th>  
+                            </thead>
+                            <body>
+                                @foreach($date as $clients)
+                                <tr>
+                                    <td>{{ $clients->name}}</td>
+                                    <td>{{ $clients->email}}</td>
+                                    <td>{{ $clients->phone}}</td>
+                                    <td>{{ $clients->address}}</td>
+                                    <td>{{ $clients->date_of_delivery}}</td>
+                                </tr>
+
+                                @endforeach
+                            </body>
+                           
+                        </table>
+                    </div>
                 </div>
+
+                
+                
+                <div class="card-footer py-4">
+                    <nav class="d-flex justify-content-end" aria-label="..."></nav>
+                </div>
+                
             </div>
         </div>
     </div>
