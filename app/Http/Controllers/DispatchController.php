ad657b2b0f2aad494f03f1292b14a5c232e0f827
Gabriel Drápela <56370678+Gdrapela@users.noreply.github.com>
2021-11-18 13:08:50 -0300

    Update DispatchController.php

--- app/Http/Controllers/DispatchController.php
+++ app/Http/Controllers/DispatchController.php
@@ -25,19 +25,22 @@ class DispatchController extends Controller
         $date = DB::connection(session()->get('database'))
                 ->table('sales')
                     ->join('clients', 'sales.client_id', '=', 'clients.id')
-                    ->select('clients.name','clients.email','clients.phone','clients.address','sales.date_of_delivery', 'clients.id')
+                    ->select('clients.name','clients.email','clients.phone','clients.address','sales.date_of_delivery','sales.id as sale')
                     ->where('sales.date_of_delivery','>', '00-00-0000') 
                     ->whereBetween('sales.date_of_delivery',[$fecha_inicial,$fecha_final])
                     ->orderBy('sales.date_of_delivery','ASC')
+                    //->groupBy('clients.id')->distinct()
                     ->get();
 
         return view('dispatch.index', compact('date','fecha_inicial','fecha_final'));
     }
   
-    public function show($id){
-        
-        $client = Client::select('clients.name','clients.document_id','clients.phone','clients.email','clients.address')
-                ->where('clients.id',$id)
+    public function show($sale){
+        $client =DB::connection(session()->get('database'))
+                ->table('clients')
+                ->join('sales','clients.id','=', 'sales.client_id')
+                ->select('clients.name','clients.document_id','clients.phone','clients.email','clients.address')
+                ->where('sales.id',$sale)
                 ->get();
         
         $watch = DB::connection(session()->get('database'))
@@ -45,12 +48,13 @@ class DispatchController extends Controller
                 ->join('products','sold_products.product_id','=','products.id')
                 ->join('sales','sold_products.sale_id','sales.id')
                 ->join('clients', 'sales.client_id', '=', 'clients.id')
-                ->select('products.category_product','products.name','sold_products.qty','sold_products.price','sold_products.total_amount')
+                ->select('products.category_product','products.name','sold_products.qty','sold_products.price','sold_products.total_amount','sales.date_of_delivery')
                 ->where('sales.date_of_delivery','<>', '00-00-0000')
-                ->where('clients.id',$id)
-                ->orderBy('sales.date_of_delivery', 'DESC')->take(1)->get();
- 
-        //dd($watch);
+                ->where('sales.id',$sale)
+                ->where('sales.date_of_delivery', '>=', Carbon::now())
+                ->get();
+                //->orderBy('sales.date_of_delivery', 'DESC')->take(1)->get();
+                //dd($watch);
         
         return view('dispatch.ver', compact('watch','client'));
     }
