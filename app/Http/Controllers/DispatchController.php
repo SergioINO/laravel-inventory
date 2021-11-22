9719b191b75f69b04230aa22d2c5cbe015fc6b7d
Gabriel Drápela <56370678+Gdrapela@users.noreply.github.com>
2021-11-22 13:52:28 -0300

    Update DispatchController.php

--- app/Http/Controllers/DispatchController.php
+++ app/Http/Controllers/DispatchController.php
@@ -53,10 +53,9 @@ class DispatchController extends Controller
                 ->where('sales.id',$sale)
                 ->where('sales.date_of_delivery', '>=', Carbon::now())
                 ->get();
-                
+
                 //->orderBy('sales.date_of_delivery', 'DESC')->take(1)->get();
                 //dd($watch);
-        
         return view('dispatch.ver', compact('watch','client'));
     }
 
